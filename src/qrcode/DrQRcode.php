d0593d639559219088cd76f09de9ef99fda294fd
hotarunohikari <59673213+hotarunohikari@users.noreply.github.com>
2020-05-01 09:16:00 +0800

    Update DrQRcode.php

--- src/qrcode/DrQRcode.php
+++ src/qrcode/DrQRcode.php
@@ -17,6 +17,7 @@ class DrQRcode
      *
      * @param $content 图片中要存储的内容
      * @param $path 图片存放路径
+     * @return string 图片存储的路径和名称
      * @throws Exception
      */
     static function png($content, $path) {
